f5d9e7522b7ee0f0b9abb25e8166538f695785ab
Jace Hensley <jace.hensley@workiva.com>
2017-11-01 08:37:07 -0700

    Update license

--- tool/dev.dart
+++ tool/dev.dart
@@ -1,3 +1,17 @@
+// Copyright 2017 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:dart_dev/dart_dev.dart' show dev, config;
 
 main(List<String> args) async {
@@ -7,7 +21,7 @@ main(List<String> args) async {
 
   config.analyze.entryPoints = const ['bin/', 'lib/', 'test/', 'tool/'];
 
-  // config.copyLicense.directories = const ['bin/', 'lib/', 'test/', 'tool/'];
+  config.copyLicense.directories = const ['bin/', 'lib/', 'test/', 'test_fixtures/', 'tool/'];
 
   config.test.unitTests = const ['test/'];
 
