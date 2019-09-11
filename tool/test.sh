d72f3a36a714cea07357f7eec67340143d3ce5d9
Olesia Thoms <olesia.thoms@workiva.com>
2019-09-11 13:55:16 -0600

    run tests in release mode

--- tool/test.sh
+++ tool/test.sh
@@ -8,7 +8,7 @@ DART_2_PREFIX="Dart VM version: 2"
 
 if [[ $DART_VERSION = $DART_2_PREFIX* ]]; then
     echo -e 'pub run build_runner test'
-    pub run build_runner test
+    pub run build_runner test --release
 else
     echo -e 'pub run test'
     pub run test
