4f3c5e82aaab47c10d3046d749e8f150ff7a9503
fzyzcjy <ch271828n@outlook.com>
2022-05-26 19:56:54 +0800

    handle more types of dependencies

--- lib/src/auto_fix.dart
+++ lib/src/auto_fix.dart
@@ -4,60 +4,123 @@ import 'package:pubspec_parse/pubspec_parse.dart';
 class AutoFix {
   final Pubspec pubspec;
 
-  final _pubAdd = <String>[];
-  final _pubAddDev = <String>[];
-  final _pubRemove = <String>[];
+  final _pubRemoveNames = <String>[];
+  final _pubAdds = <PubAddCommand>[];
 
   AutoFix(this.pubspec);
 
   void handleMissingDependencies(Set<String> deps) {
-    _pubAdd.addAll(deps);
+    _pubAdds.addAll(deps.map((dep) => PubAddCommand(packageAndConstraints: [dep], dev: false)));
   }
 
   void handleMissingDevDependencies(Set<String> deps) {
-    _pubAddDev.addAll(deps);
+    _pubAdds.addAll(deps.map((dep) => PubAddCommand(packageAndConstraints: [dep], dev: true)));
   }
 
   void handleOverPromotedDependencies(Set<String> deps) {
-    _pubRemove.addAll(deps);
-    _pubAddDev.addAll(_parseDepsWithConstraints(deps));
+    _pubRemoveNames.addAll(deps);
+    _pubAdds.addAll(_parsePubAddListByPubspec(deps, dev: true));
   }
 
   void handleUnderPromotedDependencies(Set<String> deps) {
-    _pubRemove.addAll(deps);
-    _pubAdd.addAll(_parseDepsWithConstraints(deps));
+    _pubRemoveNames.addAll(deps);
+    _pubAdds.addAll(_parsePubAddListByPubspec(deps, dev: false));
   }
 
   void handleUnusedDependencies(Set<String> deps) {
-    _pubRemove.addAll(deps);
+    _pubRemoveNames.addAll(deps);
   }
 
-  List<String> _parseDepsWithConstraints(Set<String> deps) {
-    return deps.map((dep) => _parseConstraint(dep)).where((e) => e != null).map((e) => e!).toList();
+  List<PubAddCommand> _parsePubAddListByPubspec(Set<String> deps, {required bool dev}) {
+    return deps.map((dep) => _parsePubAddByPubspec(dep, dev: dev)).where((e) => e != null).map((e) => e!).toList();
   }
 
-  String? _parseConstraint(String name) {
+  PubAddCommand? _parsePubAddByPubspec(String name, {required bool dev}) {
     final dependency = pubspec.dependencies[name] ?? pubspec.devDependencies[name];
     if (dependency == null) {
-      logger.warning('parseConstraint cannot find dependency name=$name');
+      logger.warning('WARN: cannot find dependency name=$name');
       return null;
     }
 
     if (dependency is HostedDependency) {
       final constraint = dependency.version.toString();
-      return name + ':' + constraint;
+      return PubAddCommand(packageAndConstraints: ['$name:$constraint'], dev: dev);
     }
 
-    logger.warning('parseConstraint do not know type of dependency '
+    if (dependency is PathDependency) {
+      return PubAddCommand(
+        packageAndConstraints: [name],
+        dev: dev,
+        extraArgs: '--path ${dependency.path}',
+      );
+    }
+
+    if (dependency is GitDependency) {
+      return PubAddCommand(
+        packageAndConstraints: [name],
+        dev: dev,
+        extraArgs: '--git-url ${dependency.url} --git-ref ${dependency.ref} --git-path ${dependency.path}',
+      );
+    }
+
+    logger.warning('WARN: do not know type of dependency '
         'name=$name dependency=$dependency type=${dependency.runtimeType}');
     return null;
   }
 
   String compile() {
+    final mergedPubAdds = PubAddCommand.merge(_pubAdds);
     return [
-      if (_pubRemove.isNotEmpty) 'dart pub remove ' + _pubRemove.join(' '),
-      if (_pubAdd.isNotEmpty) 'dart pub add ' + _pubAdd.join(' '),
-      if (_pubAddDev.isNotEmpty) 'dart pub add --dev ' + _pubAddDev.join(' '),
+      if (_pubRemoveNames.isNotEmpty) 'dart pub remove ' + _pubRemoveNames.join(' '),
+      ...mergedPubAdds.map((e) => e.compile()),
     ].join(' && ');
+  }
+}
+
+class PubAddCommand {
+  final bool dev;
+  final List<String> packageAndConstraints;
+  final String? extraArgs;
+
+  PubAddCommand({
+    required this.packageAndConstraints,
+    required this.dev,
+    this.extraArgs,
+  });
+
+  String compile() {
+    var ans = 'dart pub add ';
+    if (dev) ans += '--dev ';
+    ans += packageAndConstraints.join(' ') + ' ';
+    ans += extraArgs ?? '';
+    return ans;
+  }
+
+  static List<PubAddCommand> merge(List<PubAddCommand> commands) {
+    final simpleAdd = <PubAddCommand>[];
+    final simpleAddDev = <PubAddCommand>[];
+    final others = <PubAddCommand>[];
+
+    for (final command in commands) {
+      if (command.extraArgs == null) {
+        (command.dev ? simpleAddDev : simpleAdd).add(command);
+      } else {
+        others.add(command);
+      }
+    }
+
+    return [
+      if (simpleAdd.isNotEmpty)
+        PubAddCommand(
+          packageAndConstraints: simpleAdd.expand((c) => c.packageAndConstraints).toList(),
+          dev: false,
+        ),
+      if (simpleAddDev.isNotEmpty)
+        PubAddCommand(
+          packageAndConstraints: simpleAddDev.expand((c) => c.packageAndConstraints).toList(),
+          dev: true,
+        ),
+      ...others,
+    ];
   }
 }
